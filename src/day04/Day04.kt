2a8f40f7ecc1810d4f8e1786a2a03e05c7a150e7
humza <ahmadhumza7@gmail.com>
2022-12-13 05:28:20 -0800

    Added a solution for day 04 from Todd Ginsberg.

--- src/day04/Day04.kt
+++ src/day04/Day04.kt
@@ -6,6 +6,7 @@ fun main (){
     val dayFourInput = readInput("day04")
     val ranges: List<Pair<IntRange,IntRange>> = dayFourInput.map { it.asRanges() }
     println(ranges.count { it.first fullyOverlaps it.second || it.second fullyOverlaps it.first  })
+    println(ranges.count { it.first overlaps it.second })
 }
 private fun String.asIntRange(): IntRange =
     substringBefore("-").toInt() .. substringAfter("-").toInt()
@@ -14,6 +15,8 @@ private fun String.asRanges(): Pair<IntRange,IntRange> =
     substringBefore(",").asIntRange() to substringAfter(",").asIntRange()
 private infix fun IntRange.fullyOverlaps(other: IntRange): Boolean =
     first <= other.first && last >= other.last
+private infix fun IntRange.overlaps(other: IntRange): Boolean =
+    first <= other.last && other.first <= last
 
 data class Section(val start: Int, val end: Int){
     fun contains(otherSection: Section): Boolean {
