567543ad59f32eb5fb3809fa529d859d7f20b971
humza <ahmadhumza7@gmail.com>
2022-12-13 09:57:18 -0800

    Added input file for Day 06 and a solution for part one of Day 06.

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -3,5 +3,27 @@ package day06
 import readInput
 
 fun main(){
-    val input = readInput("day06.txt")
+    val input = readInput("day06")
+    val marker = input.first()
+    println(partOne(marker))
+}
+
+private fun partOne(marker: String): Int {
+    var i = 0
+
+    check@ while (i < marker.length - 3) {
+        val nextFourChars = marker.substring(i, i + 4)
+        println(nextFourChars)
+        if (nextFourChars.areAllUnique()) {
+            break@check
+        } else {
+            i++
+        }
+    }
+
+    return i + 4
+}
+
+fun String.areAllUnique(): Boolean {
+    return this.toSet().size == this.length
 }
