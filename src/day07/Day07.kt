f49b1927754685fdbd93e6987526c36861dad732
humza <ahmadhumza7@gmail.com>
2022-12-14 10:40:21 -0800

    First crack converting the terminal output into a graph.

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -3,7 +3,48 @@ package day07
 import readInput
 fun main(){
     val fileSystem = readInput("day07")
-    fileSystem.forEach {
-        println(it)
+
+    val rootDirectory = Directory(name = "root")
+    var workingDirectory = rootDirectory
+
+    fileSystem.drop(1).forEach { terminalOutput ->
+        if(terminalOutput.substring(0, 4) == "$ cd"){
+            if(terminalOutput.substringAfter("$ cd ") == ".."){
+                if(workingDirectory.parentDirectory != null){
+                    workingDirectory = workingDirectory.parentDirectory!!
+                }
+            }
+            else {
+                val directoryName = terminalOutput.substringAfter("$ cd ")
+                if(workingDirectory.subdirectories.contains(directoryName)){
+                    workingDirectory = workingDirectory.subdirectories[directoryName]!!
+                }
+            }
+        }
+
+        // Handle adding subdirectories or files
+        if(terminalOutput.substring(0, 3) == "dir"){
+            val directoryName = terminalOutput.substringAfter("dir ")
+            val childDirectory = Directory(name = directoryName)
+            childDirectory.parentDirectory = workingDirectory
+            workingDirectory.subdirectories[directoryName] = childDirectory
+        }
+        else if(terminalOutput.first().isDigit()){
+            val file = terminalOutput.split(" ")
+            val fileSize = file.first()
+            val fileName = file.last()
+            workingDirectory.files.add(File(fileName, fileSize.toInt()))
+        }
     }
+
+    println(rootDirectory)
 }
+
+data class Directory(
+    val name: String,
+    val files: MutableList<File> = mutableListOf(),
+    val subdirectories: MutableMap<String, Directory> = mutableMapOf()){
+    var parentDirectory: Directory? = null
+}
+
+data class File(val name: String, val size: Int)
