eae8edc20300ab4c34773561ff5237e27d9a9d89
humza <ahmadhumza7@gmail.com>
2022-12-25 17:42:15 -0800

    Added Todd's solution for day 10 part two.

--- src/day10/Day10ToddGinsbergSolution.kt
+++ src/day10/Day10ToddGinsbergSolution.kt
@@ -1,12 +1,15 @@
 package day10
 
 import readInput
+import kotlin.math.absoluteValue
 
 fun main() {
     val day10Input = readInput("day10")
     val signals = parseInput(day10Input).runningReduce(Int::plus)
     val dayOneSolution = solvePart1(signals)
     println(dayOneSolution)
+
+    solvePart2(signals)
 }
 
 fun parseInput(input: List<String>): List<Int> =
@@ -24,3 +27,16 @@ fun List<Int>.sampleSignals(): List<Int> =
     (60 .. size step 40).map { cycle -> cycle * this[cycle - 1] } + this[19]
 
 fun solvePart1(signals: List<Int>): Int = signals.sampleSignals().sum()
+
+fun solvePart2(signals: List<Int>): Unit = signals.screen().print()
+
+fun List<Int>.screen(): List<Boolean> = this.mapIndexed { pixel, signal -> (signal-(pixel%40)).absoluteValue <= 1 }
+
+fun List<Boolean>.print() {
+    this.windowed(40, 40, false).forEach { row ->
+        row.forEach { pixel ->
+            print(if(pixel) '#' else ' ')
+        }
+        println()
+    }
+}
