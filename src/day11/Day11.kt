20ea4c292d617f8d5fb39acae2192ac56410b3f6
humza <ahmadhumza7@gmail.com>
2022-12-28 12:36:26 -0800

    Updated day 11 to use Long instead of Int.

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -1,12 +1,15 @@
 package day11
 
 import readInput
+
 fun main (){
     val day11Input = readInput("day11")
-    println(monkeyBusiness(parseInput(day11Input)))
+    val monkeys = parseInput(day11Input)
+    println(monkeyBusiness(monkeys = monkeys, rounds = 20, worryDivisor = 3L))
+//    println(monkeyBusiness(monkeys = monkeys, rounds = 10000, worryDivisor = 0L))
 }
 
-private fun monkeyBusiness(monkeys: List<Monkey>, rounds: Int = 20, worryDivisor: Int = 3): Int {
+private fun monkeyBusiness(monkeys: List<Monkey>, rounds: Int = 20, worryDivisor: Long = 3L): Long {
     repeat(rounds) {
         monkeys.forEach { monkey ->
             // Monkey to move to - index of item to move
@@ -37,23 +40,27 @@ private fun monkeyBusiness(monkeys: List<Monkey>, rounds: Int = 20, worryDivisor
     return deviousMonkeys[0].itemsInspected * deviousMonkeys[1].itemsInspected
 }
 
-data class Monkey(val items: MutableList<Int>, val operation: Operation, val testCondition: Int, val trueMonkey: Int, val falseMonkey: Int, var itemsInspected: Int = 0){
-    fun inspectItem(itemIndex: Int, divisor : Int = 3){
-        items[itemIndex] = if(divisor > 0) items[itemIndex] / divisor else items[itemIndex]
+data class Monkey(val items: MutableList<Long>, val operation: Operation, val testCondition: Long, val trueMonkey: Int, val falseMonkey: Int, var itemsInspected: Long = 0L){
+    fun inspectItem(itemIndex: Int, divisor : Long = 3L){
+        if(divisor > 0) {
+            items[itemIndex] = items[itemIndex] / divisor
+        }
         itemsInspected++
     }
 
     fun updateWorryLevel(itemIndex: Int){
-        items[itemIndex] = operation.operate(items[itemIndex])
+        val item = items[itemIndex]
+        items[itemIndex] = operation.operate(item)
     }
 
     fun isTheHumanWorried(itemIndex: Int): Boolean {
-        return items[itemIndex] % testCondition == 0
+        val item = items[itemIndex]
+        return item % testCondition == 0L
     }
 }
 
-data class Operation(private val type: String, private val amount: Int, val useOld: Boolean){
-    fun operate(item: Int): Int {
+data class Operation(private val type: String, private val amount: Long, val useOld: Boolean){
+    fun operate(item: Long): Long {
         return if(useOld){
             when(type){
                 "*" -> item * item
@@ -76,16 +83,16 @@ fun parseInput(input: List<String>): List<Monkey> {
         // If the input is M I assume it's a Monkey
         if(input[i].isNotEmpty() && input[i].first() == 'M'){
             val startItemInputStartIndex = input[i + 1].indexOfFirst { it.isDigit() }
-            val startingItems = input[i + 1].slice(startItemInputStartIndex until input[i + 1].length).split(",").map(String::trim).map(String::toInt)
+            val startingItems = input[i + 1].slice(startItemInputStartIndex until input[i + 1].length).split(",").map(String::trim).map(String::toLong)
 //            println(startingItems)
 
             val operationStartIndex = if(input[i + 2].contains("*")) input[i + 2].indexOf("*") else input[i + 2].indexOf("+")
             val operationList = input[i + 2].slice(operationStartIndex until input[i + 2].length).split(" ")
-            val operation = Operation(type = operationList.first(), useOld = operationList.last() == "old", amount = if(operationList.last() == "old") 1 else operationList.last().toInt())
+            val operation = Operation(type = operationList.first(), useOld = operationList.last() == "old", amount = if(operationList.last() == "old") 1L else operationList.last().toLong())
 //            println(operation)
 
             val testStartIndex = input[i + 3].indexOfFirst { it.isDigit() }
-            val testAmount = input[i + 3].slice(testStartIndex until input[i + 3].length).toInt()
+            val testAmount = input[i + 3].slice(testStartIndex until input[i + 3].length).toLong()
 //            println(testAmount)
 
             val trueStartIndex = input[i + 4].indexOfFirst { it.isDigit() }
