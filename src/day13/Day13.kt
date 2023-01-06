fef291c511408c1a6f34a24ed7070ceabc0894a1
humza <ahmadhumza7@gmail.com>
2023-01-06 11:39:12 -0800

    Added day 13 sample input. Added a packet class with left and right inputs.

--- src/day13/Day13.kt
+++ src/day13/Day13.kt
@@ -4,4 +4,15 @@ import readInput
 
 fun main(){
     val day13Input = readInput("day13")
+    val packets = parseInput(day13Input)
+    packets.forEach(::println)
 }
+
+fun parseInput(input: List<String>): List<Packet> {
+    val pairs = input.filter { it.isNotBlank() }.chunked(2)
+    return pairs.map {
+        Packet(it.first(), it.last())
+    }
+}
+
+data class Packet(val left: String, val right: String)
